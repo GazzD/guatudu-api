bfddd642616c913902e6437ba9a8b373d150973c
AnibalCardozo <anibal@sappitotech.com>
2020-01-18 19:20:32 -0400

    fix migrations

--- api/api/locations/models/countries.py
+++ api/api/locations/models/countries.py
@@ -6,14 +6,10 @@ from django.db import models
 # Utilities
 from api.utils.models import BaseModel
 
-# Models
-from api.locations.models.cities import City
-
 class Country(BaseModel):
     """ Country model. """
     name = models.CharField(max_length = 150)
     image = models.ImageField(upload_to='cities/images', max_length=100)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
     
     def __str__(self):
         """ Return country string representation"""
