ab42d10cffdaef389be7869ab0ceaa08c87fdc58
AnibalCardozo <anibal@sappitotech.com>
2020-01-20 01:10:46 -0400

    debug

--- api/api/locations/urls.py
+++ api/api/locations/urls.py
@@ -9,7 +9,7 @@ from rest_framework.routers import DefaultRouter
 # Views
 from api.locations.views import countries as country_views
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(r'countries', country_views.CountryViewSet, basename='country')
 
 urlpatterns = router.urls
